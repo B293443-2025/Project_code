2d676ac7ed149bd90b19fdab2c7b6b9ba852a511
B293443-2025 <s2850039@ed.ac.uk>
2026-08-17 19:19:02 +0100

    Update custom_nab3_pipeline.sh

--- motif_prediction/ClipGPS/custom_nab3_pipeline.sh
+++ motif_prediction/ClipGPS/custom_nab3_pipeline.sh
@@ -1,13 +1,3 @@
-python scripts/prepare_from_gtf.py \
---gtf ../Pysster/Nab3_dataset_100nt/test_count_output_FDRs_r100_norm100_no_overlaps.gtf \
---annotation-gtf ../../data/references/GSE276517_Saccharomyces_cerevisiae.R64-1-1.75_1.2.gtf \
---genome ../../data/references/*fa \
---chromsizes ../../data/references/*txt \
---protein nab3 \
---out datasets/nab3_100nt_clip.parquet \
---neg-gtf ../Pysster/Nab3_dataset_100nt/test_count_output_FDRs_r100_norm100_negative_controls.gtf \
---length 100 \
---no-depfold
 
 CHECKPOINT=nab3_100nt_fit/tune_pass2/d0.1_lr3e-4_chosen/best_model.pt
 DATA=datasets/nab3_100nt_clip.parquet
